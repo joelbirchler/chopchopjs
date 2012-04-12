55356098ae43f51eafaf779d1e36dbaa6a069588
Joel Birchler <joel@joelbirchler.com>
2012-04-11 21:16:05 -0700

    improved the bear and fish puzzle

--- javascripts/puzzles.js
+++ javascripts/puzzles.js
@@ -4,7 +4,7 @@ $(function() {
     "var foo = 2 + chicken();\n#var chicken = function() {return 2;}\nassert(foo, 4);",
     "var foo = [8,1,5,3,1];\ntheMin = Math.min.#\nassert(theMin, 1);",
     "var meow = function() {\n\tvar answer = undefined;\n\tvar args = #arguments;\n\treturn args.pop();\n}\n\nvar answer = Math.random();\nvar cats = meow(answer);\nassert(cats, answer);",
-    "var that = this, bear = 0, fish = {}, river = [Math.random(),2,3,4,5];\nvar flow = function(obj, iterator, context) {\n\tfor (var i = 0, l = obj.length; i < l; i++) {\n\t\tif (iterator.call(context, obj[i], i) === fish) return;\n\t}\n}\nflow(river, function(w,t) {\n\tvar river = undefined\n\tbear += w;\n\t#\n}, that);\nassert(bear, river[0] + 5);"
+    "var bear = 0, fish = {}, river = [];\nfor (var i = 0; i < 10; i++) {\n\triver[i] = Math.random();\n}\nvar flow = function(obj, iterator, context) {\n\tfor (var j = 0, l = obj.length; j < l; j++) {\n\t\tif (iterator.call(context, obj[j], j) === fish) return;\n\t}\n}\nflow(river, function(w,t) {\n\tvar river = undefined\n\tbear += w;\n\t#\n}, this);\nassert(bear, river[0] + river[1] + river[2]);"
   );
 
   Backbone.history.start();
